febd678b23a1bbf785f750c1e1837d7d85486736
Alex <contact.alexsoyes@gmail.com>
2024-07-03 15:49:17 +0200

    chore(aliases): add aiddc-commits-diff-main alias

--- scripts/aiddc/aliases.sh
+++ scripts/aiddc/aliases.sh
@@ -9,6 +9,7 @@ alias aiddc-review='~/.aiddc/scripts/aidd-review.sh'
 # Commits
 alias aiddc-commit-msg='~/.aiddc/scripts/aidd-commit-msg.sh'
 alias aiddc-commit-last='~/.aiddc/scripts/aidd-commit-last.sh'
+alias aiddc-commits-diff-main='~/.aiddc/scripts/aidd-commits-diff-main.sh'
 
 # Scripts
 alias aiddc-changes='~/.aiddc/scripts/aidd-changes.sh'
